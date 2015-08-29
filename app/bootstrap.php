73027f74f2d378cb007a84b9c9ac37aab9abdca9
chemiX <iam@chemix.cz>
2015-08-29 15:06:31 +0200

    Misc: add dump&exit shortcut

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/misc/functions.php';
 
 $configurator = new Nette\Configurator;
 
---
+++ app/misc/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Honza Cerny (http://honzacerny.com)
+ */
+
+/**
+ * Dump & Exit
+ *
+ * @param $var
+ */
+function de($var){
+	dump($var);
+	exit();
+}
