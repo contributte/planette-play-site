90695f04532cd16ba3b047e8b30e773bdc6d62ae
chemiX <iam@chemix.cz>
2016-01-30 01:51:49 +0100

    add about page

---
+++ app/FrontModule/presenters/AboutPresenter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Honza Cerny (http://honzacerny.com)
+ */
+
+namespace App\FrontModule\Presenters;
+
+class AboutPresenter extends BasePresenter
+{
+
+	public function renderDefault()
+	{
+	}
+
+}
